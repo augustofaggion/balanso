70a1b2404537612fa6d7eb5618b09167535bed46
Augusto Faggion <a.faggion.santos@gmail.com>
2025-10-22 22:45:45 +0200

    feat changed structure for a home page

---
+++ src/app/(pages)/budget/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Tracker from '@/app/components/Tracker';
+import { Euro } from 'lucide-react';
+
+const BudgetPage = () => {
+  return (
+    <div className="bg-gradient-to-br from-purple-900 via-blue-900 to-indigo-900 min-h-screen">
+      <Tracker 
+      title="Budget"
+      icon={<Euro className="w-8 h-8 text-white"/>}
+      localStorageKey="budget"  
+      currencyCode="EUR"
+      currencySymbol="€"
+      addLabel="Add"
+      removeLabel="Remove"
+      resetLabel="Reset Budget"
+      />
+    </div>
+  );
+};
+
+export default BudgetPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,6 @@ import { Euro } from "lucide-react";
 
 export default function Home() {
   return (
-      <div className="min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-indigo-900">
-      <Tracker 
-      title="Budget"
-      icon={<Euro className="w-8 h-8 text-white"/>}
-      localStorageKey="budget"  
-      currencyCode="EUR"
-      currencySymbol="€"
-      addLabel="Add"
-      removeLabel="Remove"
-      resetLabel="Reset Budget"
-      />
-    </div>
+        <div> In maintenance</div>
   );
 }
